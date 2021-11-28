af444c11523378db8920af46be42f8c1166c5173
Jair Gómez Vásquez <36084661+Jairgo@users.noreply.github.com>
2021-11-28 15:32:04 -0600

    Update Backends

--- core/backends.py
+++ core/backends.py
@@ -14,10 +14,11 @@ class UserBackend(ModelBackend):
             except User.DoesNotExist:
                 print("Not an existing user")
             return user
-        def get_user(self,user_id):
-            try:
-                return User.objects.get(pk= user_id)
-            except User.DoesNotExist:
-                return None
+            
+    def get_user(self,user_id):
+        try:
+            return User.objects.get(pk= user_id)
+        except User.DoesNotExist:
+            return None
         
         
